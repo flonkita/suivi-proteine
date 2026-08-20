3e9f7c818a2e77285703d191107ac54547f9aad5
florent.nkita <flo95400@gmail.com>
2026-08-21 01:00:59 +0200

    Update API URL to production

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // On utilise l'IP locale de ta machine et le port de ton backend
 const api = axios.create({
-  baseURL: "http://192.168.1.114:3003/api",
+  baseURL: "https://protein-tracker-api-illd.onrender.com/api", // Utilise l'URL de ton backend sur Render
   timeout: 5000, // L'application arrête de chercher après 5 secondes si le back est éteint
   headers: {
     "Content-Type": "application/json",
